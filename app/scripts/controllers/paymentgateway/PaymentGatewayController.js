2e57396efb216ffbd96bb833686ec7f3e3740db8
ashokreddylingala <lingala.ashokreddy@gmail.com>
2014-01-10 09:43:18 +0530

    addPagingToPaymentGateway.html

--- app/scripts/controllers/paymentgateway/PaymentGatewayController.js
+++ app/scripts/controllers/paymentgateway/PaymentGatewayController.js
@@ -3,7 +3,7 @@
 	  PaymentGatewayController: function(scope,webStorage, routeParams,location, resourceFactory, paginatorService) {
 		  
         scope.paymentgatewaydatas = [];
-
+        
         var callingTab = webStorage.get('callingTab',null);
         if(callingTab == null){
         	callingTab="";
@@ -19,14 +19,38 @@
 		  };
 		 
         }
-        
-
-
-
-        resourceFactory.paymentGatewayResource.get(function(data) {
-        	 scope.paymentgatewaydatas=data; 
-        });
-
+	  		 scope.paymentGatewayFailureData = function(offset, limit, callback) {
+	  			 resourceFactory.paymentGatewayResource.get({offset: offset, limit: limit, tabType: 'Failure'} , callback);
+	 		};
+	  		
+	  		scope.getPaymentGateway = function () {
+	        	scope.paymentgatewaydatas = paginatorService.paginate(scope.paymentGatewayFailureData, 14);
+	        };
+	        
+	        scope.getPaymentGatewaySuccess = function () {
+	        	scope.paymentgatewaydatas = paginatorService.paginate(scope.paymentGatewaySuccessData, 14);
+	        };
+	        scope.paymentGatewaySuccessData = function(offset, limit, callback) {
+	  			 resourceFactory.paymentGatewayResource.get({offset: offset, limit: limit, tabType: 'Success'} , callback);
+	 		};
+	 		
+	 		scope.searchFailedPaymentData = function(offset, limit, callback) { 
+		    	  resourceFactory.paymentGatewayResource.get({offset: offset, limit: limit, 
+		    		  sqlSearch: scope.filterText, tabType: 'Failure'} , callback);
+		          };
+		  		
+		  	scope.searchFailedPaymentId = function(filterText) {
+		  			scope.paymentgatewaydatas = paginatorService.paginate(scope.searchFailedPaymentData, 14);
+		  		};
+		  		
+		  	scope.searchSuccessPaymentData = function(offset, limit, callback) { 
+			    	  resourceFactory.paymentGatewayResource.get({offset: offset, limit: limit, 
+			    		  sqlSearch: scope.filterText, tabType: 'Success'} , callback);
+			          };
+			  		
+		    scope.searchSuccessPaymentId = function(filterText) {
+			  			scope.paymentgatewaydatas = paginatorService.paginate(scope.searchSuccessPaymentData, 14);
+			  		};		
         
     }
   });
