ec2fcd5dfdf8bce2a1305babd54c0bb07409db92
lalsing161 <lalsingh161@gmail.com>
2014-01-23 14:43:23 +0530

    bug fixing issues cleared

--- app/scripts/controllers/organization/ViewPlanController.js
+++ app/scripts/controllers/organization/ViewPlanController.js
@@ -1,6 +1,6 @@
 (function(module) {
   mifosX.controllers = _.extend(module, {
-	  ViewPlanController: function(scope, routeParams , location,resourceFactory ) {
+	  ViewPlanController: function(scope, routeParams , location,resourceFactory,$modal ) {
         scope.plan = [];
         resourceFactory.planResource.get({planId: routeParams.id} , function(data) {
             scope.plan = data;
@@ -8,14 +8,30 @@
         });
         
         scope.deleteplan=function(){
-        	
-        resourceFactory.planResource.delete({'planId':routeParams.id},{},function(data){
-            location.path('/plans');
-        });
+        
+            $modal.open({
+                templateUrl: 'approve.html',
+                controller: Approve,
+                resolve:{}
+            });
         }
+        
+        var Approve = function ($scope, $modalInstance) {
+            $scope.approve = function (act) {
+                scope.approveData = {};
+                resourceFactory.planResource.delete({'planId':routeParams.id},{},function(data){
+                    location.path('/plans');
+                });
+                $modalInstance.close('delete');
+            };
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        };
+        
     }
   });
-  mifosX.ng.application.controller('ViewPlanController', ['$scope', '$routeParams', '$location','ResourceFactory', mifosX.controllers.ViewPlanController]).run(function($log) {
+  mifosX.ng.application.controller('ViewPlanController', ['$scope', '$routeParams', '$location','ResourceFactory','$modal', mifosX.controllers.ViewPlanController]).run(function($log) {
     $log.info("ViewPlanController initialized");
   });
 }(mifosX.controllers || {}));
