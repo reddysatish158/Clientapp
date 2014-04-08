2dd6e3d7bf0653a00fb4dda7517b056a8f19c2d0
Rakesh <rakesh.adlagatta@gmail.com>
2014-04-08 18:59:53 +0530

    added close client

---
+++ app/scripts/controllers/client/ClientCloseController.js
@@ -0,0 +1,50 @@
+(function(module) {
+  mifosX.controllers = _.extend(module, {
+ClientCloseController: function(scope,webStorage, resourceFactory, location, translate,dateFilter,routeParams) {
+            
+
+scope.formData={};	
+
+scope.close = {};
+scope.close.date = new Date();
+scope.minDate= scope.close.date;
+
+scope.clientId=routeParams.id;
+var clientData = webStorage.get('clientData');
+scope.displayName=clientData.displayName;
+scope.hwSerialNumber=clientData.hwSerialNumber;
+scope.statusActive=clientData.statusActive;
+scope.accountNo=clientData.accountNo;
+scope.officeName=clientData.officeName;
+scope.balanceAmount=clientData.balanceAmount;
+scope.currency=clientData.currency;
+scope.imagePresent=clientData.imagePresent;
+scope.categoryType=clientData.categoryType;
+scope.email=clientData.email;
+scope.phone=clientData.phone;
+
+
+// TEMPLATE DATA
+resourceFactory.clientResource.get({clientId: routeParams.id}, function (data) {
+                    
+//data
+                });
+            
+           
+scope.submit = function() {
+this.formData.locale = 'en';
+var reqDate = dateFilter(scope.close.date,'dd MMMM yyyy');
+this.formData.closeDate = reqDate;
+this.formData.dateFormat = 'dd MMMM yyyy';
+
+                resourceFactory.clientResource.save({'clientId': routeParams.id},this.formData,function(data){
+                
+                 location.path('/viewTicket/'+ routeParams.id);
+               });
+         };
+    }
+  });
+  mifosX.ng.application.controller('ClientCloseController', ['$scope', 'webStorage','ResourceFactory', '$location', '$translate','dateFilter', '$routeParams', mifosX.controllers.ClientCloseController]).run(function($log) {
+    $log.info("ClientCloseController initialized");
+  });
+}(mifosX.controllers || {}));
